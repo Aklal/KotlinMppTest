2a2fe7fb10f8fad8b076a3b21f7b83f244c75db2
aklal <oltreb@yahoo.fr>
2020-05-28 16:06:50 +0200

    Change references to use buildSrc

--- MppTest/build.gradle.kts
+++ MppTest/build.gradle.kts
@@ -1,29 +1,34 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.71'
     repositories {
         google()
+        mavenCentral()
         jcenter()
-        
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.0.0-rc01'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath "org.jetbrains.kotlin:kotlin-serialization:$kotlin_version"
+        classpath(Deps.android_gradle_plugin)
+        classpath(Deps.SqlDelight.gradle)
+        classpath(Deps.cocoapodsext)
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
+
+        classpath(kotlin("gradle-plugin", Versions.kotlin))
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build gradle files
     }
 }
 
 allprojects {
     repositories {
         google()
+        mavenCentral()
         jcenter()
-        
+        maven(url = "https://kotlin.bintray.com/kotlinx")
+        maven(url = "https://dl.bintray.com/ekito/koin")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }

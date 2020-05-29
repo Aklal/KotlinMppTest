35682e2e9fa9eb31fce5b48d353a343b3aa9df52
aklal <oltreb@yahoo.fr>
2020-05-29 10:29:07 +0200

    Update minSdk and android gradle plugins versions

--- MppTest/buildSrc/src/main/java/Dependencies.kt
+++ MppTest/buildSrc/src/main/java/Dependencies.kt
@@ -1,12 +1,12 @@
 object Versions {
-    val min_sdk = 2
+    val min_sdk = 15
     val target_sdk = 29
     val compile_sdk = 29
 
     val kotlin = "1.3.72"
     val androidx_test = "1.2.0"
     val androidx_test_ext = "1.1.1"
-    val android_gradle_plugin = "3.6.3"
+    val android_gradle_plugin = "4.0.0"
     val buildToolsVersion = "29.0.0"
     val junit = "4.13"
     val sqlDelight = "1.3.0"
